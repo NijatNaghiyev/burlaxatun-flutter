98e182faffba5b567d509d30d8a1beed96c036a3
UzeyirN <7qzjlm4@code.edu.az>
2025-05-05 21:59:31 +0400

    - Contact us is done

--- lib/utils/helper/url_launcher_helper.dart
+++ lib/utils/helper/url_launcher_helper.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:url_launcher/url_launcher.dart';
 
 class UrlLauncherHelper {
+  /// Call phone number method
   static Future<void> launchPhoneCall(String phoneNumber) async {
     final Uri uri = Uri(scheme: 'tel', path: phoneNumber);
     if (await canLaunchUrl(uri)) {
@@ -10,17 +13,42 @@ class UrlLauncherHelper {
     }
   }
 
+  /// Write in whatsapp method
+  // static Future<void> launchWhatsApp(String phoneNumber,
+  //     {String message = ''}) async {
+  //   final Uri uri = Uri.parse(
+  //       'https://wa.me/$phoneNumber?text=${Uri.encodeComponent(message)}');
+  //   if (await canLaunchUrl(uri)) {
+  //     await launchUrl(uri);
+  //   } else {
+  //     throw 'Could not launch $uri';
+  //   }
+  // }
+
   static Future<void> launchWhatsApp(String phoneNumber,
       {String message = ''}) async {
     final Uri uri = Uri.parse(
         'https://wa.me/$phoneNumber?text=${Uri.encodeComponent(message)}');
+
     if (await canLaunchUrl(uri)) {
-      await launchUrl(uri);
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
     } else {
-      throw 'Could not launch $uri';
+      // If Whatsapp is not exist
+      final Uri storeUri = Platform.isAndroid
+          ? Uri.parse(
+              'https://play.google.com/store/apps/details?id=com.whatsapp')
+          : Uri.parse(
+              'https://apps.apple.com/app/whatsapp-messenger/id310633997');
+
+      if (await canLaunchUrl(storeUri)) {
+        await launchUrl(storeUri, mode: LaunchMode.externalApplication);
+      } else {
+        throw 'Could not launch WhatsApp or store link.';
+      }
     }
   }
 
+  /// Write in mail method
   static Future<void> launchEmail({
     required String toEmail,
     String subject = '',
