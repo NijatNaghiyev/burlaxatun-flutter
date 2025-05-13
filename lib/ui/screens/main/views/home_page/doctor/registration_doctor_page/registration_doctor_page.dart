e1a0ae94beaa4b3f7ae2dae1063070ba82bee45e
UzeyirN <7qzjlm4@code.edu.az>
2025-05-13 15:16:45 +0400

    - Doctor reservation time parsed

--- lib/ui/screens/main/views/home_page/doctor/registration_doctor_page/registration_doctor_page.dart
+++ lib/ui/screens/main/views/home_page/doctor/registration_doctor_page/registration_doctor_page.dart
@@ -5,6 +5,7 @@ import 'package:go_router/go_router.dart';
 import '../../../../../../../cubits/doctors_detail/doctors_detail_cubit.dart';
 import '../../../../../../../utils/constants/color_constants.dart';
 import '../../../../../../../utils/extensions/num_extensions.dart';
+import '../../../../../../../utils/helper/doctor_time_helper.dart';
 import '../../../../../../widgets/global_button.dart';
 import 'widgets/registration_date_and_time_widget.dart';
 import 'widgets/registration_doctor_info.dart';
@@ -12,6 +13,7 @@ import 'widgets/registration_price_and_time.dart';
 
 class RegistrationDoctorPage extends StatelessWidget {
   final String slug;
+
   const RegistrationDoctorPage({super.key, required this.slug});
 
   @override
@@ -52,18 +54,25 @@ class RegistrationDoctorPage extends StatelessWidget {
                     return Center(child: Text('Həkim məlumatı tapılmadı.'));
                   }
 
+                  final List<String> timeList = doctor.availableTime != null
+                      ? DoctorTimeHelper.generateHourlyTimes(
+                          doctor.availableTime!)
+                      : [];
+
                   return Column(
                     children: [
                       RegistrationDoctorInfo(doctor: doctor),
                       32.h,
                       RegistrationPriceAndTime(doctor: doctor),
+                      40.h,
+                      RegistrationDateAndTimeWidget(
+                        timeList: timeList,
+                      ),
+                      75.h,
                     ],
                   );
                 },
               ),
-              40.h,
-              RegistrationDateAndTimeWidget(),
-              75.h,
             ],
           ),
         ),
