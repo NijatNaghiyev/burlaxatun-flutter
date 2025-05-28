90932de4de120f59b85d51615e204145f22df31c
nihadzzz <nihadosmanov21@gmail.com>
2025-05-28 22:50:11 +0400

    - fixed register questions

--- lib/data/services/remote/user_update_service.dart
+++ lib/data/services/remote/user_update_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:burla_xatun/data/models/remote/response/user_update_model.dart';
+import 'package:burla_xatun/data/services/local/login_token_service.dart';
 import 'package:burla_xatun/data/services/local/register_token_service.dart';
 import 'package:burla_xatun/data/services/remote/base_network_service.dart';
 import 'package:burla_xatun/utils/constants/endpoints_constants.dart';
@@ -21,7 +22,7 @@ class UserUpdateService {
     String? activeLanguage,
     bool? enableNotifications,
   }) async {
-    final token = locator<RegisterTokenService>().token;
+    final token = locator<LoginTokenService>().token;
     final endpoint = EndpointsConstants.userUpdate;
 
     final formData = FormData.fromMap({
